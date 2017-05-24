07012c968713b7638e489146ede6ad4827100fb1
mc <mc@MC09.local>
2017-05-24 11:55:01 -0400

    Added project files

---
+++ script.js
@@ -0,0 +1,27 @@
+var input = [319,680,180,690,129,620,762,689,762,318,368,710,720,710,629,168,160,689,716,731,736,729,316,729,729,710,769,290,719,680,318,389,162,289,162,718,729,319,790,680,890,362,319,760,316,729,380,319,728,716];
+input = input.sort();
+input = input.filter(function(element, index, array) {
+  return element != array[index-1];
+});
+for(var element in input) {
+  input[element] = input[element].toString();
+}
+
+var map = {};
+for(var element of input) {
+  var match1 = [];
+  var match2 = [];
+  var regex1 = new RegExp("\^" + element.slice(-1));
+  var regex2 = new RegExp("\^" + element.slice(-2));
+  for(var element2 of input) {
+    if(element == element2) continue;
+    if(regex1.exec(element2)) match1.push(element2);
+    if(regex2.exec(element2)) match2.push(element2);
+  }
+  map[element] = {1: match1, 2: match2};
+}
+for(var element of map) {
+  
+}
+
+var ans = JSON.stringify(map);
