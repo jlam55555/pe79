c76b8f51dddbc4721fbe40f09c2c98d1661a0e8d
mc <mc@JBHSC21002.local>
2017-05-25 17:47:27 -0400

    Added thought

--- script.js
+++ script.js
@@ -6,24 +6,72 @@ input = input.filter(function(element, index, array) {
 for(var element in input) {
   input[element] = input[element].toString();
 }
-
-var map = {};
+var loners = {};
 for(var element of input) {
-  var match1 = [];
-  var match2 = [];
-  var regex1 = new RegExp("\^" + element.slice(-1));
-  var regex2 = new RegExp("\^" + element.slice(-2));
+  var matched = false;
   for(var element2 of input) {
-    if(element == element2) continue;
-    if(regex1.exec(element2)) match1.push(element2);
-    if(regex2.exec(element2)) match2.push(element2);
+    if(element.slice(-2) == element2.slice(0, 2)) {
+      matched = true;
+      break;
+    }
   }
-  map[element] = {match1: match1, match2: match2};
+  if(!matched) {
+    loners[element] = [element];
+  }
 }
-for(var element of map) {
-  if(element.match1.length == 0 && element.match2.length == 0) {
-    console.log(element);
+for(var i = 0; i < 5; i++) {
+  for(var element of input) {
+    for(var element2_number in loners) {
+      var element2 = loners[element2_number];
+      if(element2_number == element || element2.indexOf(element) >= 0) continue;
+      if(element.slice(-2) == element2_number.slice(0, 2)) {
+        loners[element + element2_number.slice(2)] = element2.concat([element]);
+      } else if(element.slice(-1) == element2_number.slice(0, 1)) {
+        loners[element + element2_number.slice(1)] = element2.concat([element]);
+      }
+    }
   }
 }
 
-var ans = JSON.stringify(map);
+var nonloners = {};
+for(var element of input) {
+  if(loners[element] == undefined) {
+    nonloners[element] = [element];
+  }
+}
+for(var element1 in nonloners) {
+  for(var element2 in nonloners) {
+    if((new Set(nonloners[element1].concat(nonloners[element2]))).size == nonloners[element1].concat(nonloners[element2]).length) {
+      if(element1.slice(-2) == element2.slice(0, 2)) {
+        nonloners[element1 + element2.slice(2)] = nonloners[element2].concat(nonloners[element1]);
+      } else if(element1.slice(-1) == element2.slice(0, 1)) {
+        nonloners[element1 + element2.slice(1)] = nonloners[element2].concat(nonloners[element1]);
+      }
+    }
+  }
+}
+for(var element1 in nonloners) {
+  for(var element2 in loners) {
+    if((new Set(nonloners[element1].concat(loners[element2]))).size == nonloners[element1].concat(loners[element2]).length) {
+      if(element1.slice(-2) == element2.slice(0, 2)) {
+        nonloners[element1 + element2.slice(2)] = loners[element2].concat(nonloners[element1]);
+      } else if(element1.slice(-1) == element2.slice(0, 1)) {
+        nonloners[element1 + element2.slice(1)] = loners[element2].concat(nonloners[element1]);
+      }
+    }
+  }
+}
+
+for(var obj in nonloners) {
+  loners[obj] = nonloners[obj];
+}
+
+var keys = Object.keys(loners);
+keys = keys.sort(function(a, b) { return b.length-a.length; });
+var lonerString = "";
+for(var key of keys) {
+  loners[key].sort();
+  lonerString += "<br>Length: " + key.length + ", Key: " + key + ", Contains: (" + loners[key].length + ") " + loners[key];
+}
+
+var ans = input + "<br>" + lonerString;
